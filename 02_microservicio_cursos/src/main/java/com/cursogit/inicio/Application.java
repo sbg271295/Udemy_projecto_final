281e359be8178e3b81c46c050e12edb2de4bcb68
Sergio Bravo <s.bravoga1995@gmail.com>
2024-10-31 10:53:17 +0100

    logica de negocio por user 1

--- 02_microservicio_cursos/src/main/java/com/cursogit/inicio/Application.java
+++ 02_microservicio_cursos/src/main/java/com/cursogit/inicio/Application.java
@@ -3,7 +3,8 @@ package com.cursogit.inicio;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackages = "service")
 public class Application {
 
 	public static void main(String[] args) {
